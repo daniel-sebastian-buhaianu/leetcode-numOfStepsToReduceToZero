b562ccbbf9e098d7744a22801fe7cbce0c5c5d8b
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-18 13:22:32 +0300

    implemented solution

--- main.kt
+++ main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    
+    println(numberOfSteps(8))
+}
+
+fun numberOfSteps(num: Int): Int {
+    var steps = 0
+    var tmp = num
+    while (tmp != 0) {
+        if (tmp % 2 == 0) {
+            tmp = tmp / 2
+        } else {
+            tmp = tmp - 1
+        }
+        steps++
+    }
+    return steps
 }
